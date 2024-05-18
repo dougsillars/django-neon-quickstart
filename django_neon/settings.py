1c45ecac998f2a3cc2a6d925f23791bbea8d2b11
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2024-05-18 16:46:12 -0400

    vercel data

--- django_neon/settings.py
+++ django_neon/settings.py
@@ -133,9 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
