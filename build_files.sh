052cf6a14b9dbfffb689d31d87506726ea4232e9
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2024-05-18 17:13:52 -0400

    udpate

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 # build_files.sh
 
 pip install -r requirements.txt
-python3.12.1 manage.py runserver --noinput 
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
